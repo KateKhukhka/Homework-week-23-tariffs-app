c13fec175b86abb309d07d7e7bc47ef97f34f8e1
KateKhukhka <khukhka@mail.ru>
2025-04-07 23:05:45 +0300

    add map method for cards

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,12 +1,17 @@
 import Card from "./tariffCard";
 
 function Content() {
+  const cards = [
+    { name: 300, speed: 10, remark: "Обьем включенного трафика не ограничен" },
+    { name: 450, speed: 50, remark: "Обьем включенного трафика не ограничен" },
+    { name: 550, speed: 100, remark: "Обьем включенного трафика не ограничен" },
+    { name: 1000, speed: 200, remark: "Обьем включенного трафика не ограничен" },
+  ];
   return (
     <div className="content">
-      <Card price_name="300" speed="10" remark="Обьем включенного трафика не ограничен" />
-      <Card price_name="450" speed="50" remark="Обьем включенного трафика не ограничен" />
-      <Card price_name="550" speed="100" remark="Обьем включенного трафика не ограничен" />
-      <Card price_name="1000" speed="200" remark="Обьем включенного трафика не ограничен" />
+      {cards.map((item, index) => {
+        return <Card key={index} price_name={item.name} speed={item.speed} remark={item.remark} />;
+      })}
     </div>
   );
 }
