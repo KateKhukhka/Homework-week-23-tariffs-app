43f2cd9487707927c0fcc7c81a23e43e310fbc87
KateKhukhka <khukhka@mail.ru>
2025-04-10 01:34:43 +0300

    separate card data

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,19 +1,11 @@
 import Card from "./tariffCard";
+import cardData from "../cardData";
 
 function Content() {
-  const cards = [
-    { name: 300, speed: 10, remark: "Обьем включенного трафика не ограничен" },
-    { name: 450, speed: 50, remark: "Обьем включенного трафика не ограничен" },
-    { name: 550, speed: 100, remark: "Обьем включенного трафика не ограничен" },
-    { name: 1000, speed: 200, remark: "Обьем включенного трафика не ограничен" },
-  ];
-
   return (
     <div className="content">
-      {cards.map((item, index) => {
-        return (
-          <Card key={index} price_name={item.name} speed={item.speed} remark={item.remark} />
-        );
+      {cardData.map((item, index) => {
+        return <Card key={index} price_name={item.name} speed={item.speed} remark={item.remark} />;
       })}
     </div>
   );
